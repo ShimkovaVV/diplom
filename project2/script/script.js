ca5edf15c2aaed593e159c6d71bf7de70a464b57
ShimkovaVV <109003039+ShimkovaVV@users.noreply.github.com>
2022-08-15 17:57:49 +0300

    Add files via upload

--- project2/script/script.js
+++ project2/script/script.js
@@ -4,3 +4,31 @@ $('.about_items > div').on('click', function () {
 	$(this).next('.about-text').slideToggle(300);
 
 })
+
+$('.features_item > div').on('click', function () {
+	$(this).children('.features_pretitle').toggleClass('change-color-text');
+	$(this).next('.features_text').slideToggle(300);
+
+})
+
+//https:api.telegram.org/bottoken/getUpdates
+
+$('#button').on('click', function (e) {
+	e.preventDefault();
+
+	let email = $('#email').val().trim();
+
+	if (email == "") {
+		$('#error-email').text('Enter your email')
+	}
+	$.ajax({
+		url: 'ajax/telegram.php',
+		type: 'POST',
+		cashe: false,
+		data: { 'email': email },
+		dataType: 'html',
+		succes: function () {
+			alert('its okey')
+		}
+	})
+})
